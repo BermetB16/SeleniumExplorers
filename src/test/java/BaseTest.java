e1384a5bf6542e949cb4f30dc39fdc7ca921728f
Nurda <nurdan.kazakbaev@gmail.com>
2025-01-28 13:38:48 +0600

    nurdan's right commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,15 @@
 import org.openqa.selenium.interactions.Actions;
 import talentLMS.driver.Driver;
+import talentLMS.entity.Course;
 import talentLMS.entity.User;
 import talentLMS.helper.WebElementActions;
 import talentLMS.page.BasePage;
+import talentLMS.page.courses.AddCoursePage;
 import talentLMS.page.dashboard.DashboardPage;
 import talentLMS.page.login.LoginPage;
 import talentLMS.page.users.AddCategoryPage;
 import talentLMS.page.users.AddUserPage;
+import talentLMS.utils.randomEntityUtils.RandomCourseGenerator;
 import talentLMS.utils.randomEntityUtils.RandomUserGenerator;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeSuite;
@@ -18,9 +21,12 @@ import org.testng.annotations.BeforeSuite;
         DashboardPage dashboardPage;
         AddUserPage addUserPage;
         RandomUserGenerator randomUserGenerator;
+        RandomCourseGenerator randomCourseGenerator = new RandomCourseGenerator();
         Actions actions;
         User randomUser;
         AddCategoryPage addCategoryPage = new AddCategoryPage();
+        AddCoursePage addCoursePage = new AddCoursePage();
+        Course randomCourse = randomCourseGenerator.randomCourse();
 
         @BeforeSuite
         public void beforeSuite() {
