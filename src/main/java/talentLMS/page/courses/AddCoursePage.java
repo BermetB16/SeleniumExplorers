129fccb1f275cb22a1cfe9d406cd74b21914faab
Nurda <nurdan.kazakbaev@gmail.com>
2025-02-04 16:41:00 +0600

    Nuran's Changes

--- src/main/java/talentLMS/page/courses/AddCoursePage.java
+++ src/main/java/talentLMS/page/courses/AddCoursePage.java
@@ -4,8 +4,15 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import talentLMS.entity.Course;
 import talentLMS.page.base.BasePage;
+import talentLMS.page.dashboard.legacy.AdminDashboardPage;
+import talentLMS.utils.enums.Page;
 
 public class AddCoursePage extends BasePage {
+
+    AdminDashboardPage adminDashboardPage = new AdminDashboardPage();
+
+    @FindBy(css = "div.btn-group > a.btn.btn-primary[href*='/course/create")
+    public WebElement addCourseButton;
 
     @FindBy(xpath = "//div[@class='controls']/div[@class='input-append tl-countdown']/input[@name='name']")
     public WebElement courseNameField;
@@ -25,13 +32,14 @@ public class AddCoursePage extends BasePage {
     @FindBy(css = "#tl-confirm-submit")
     public WebElement deleteButton;
 
-//
-//    public AddCoursePage addCourse(Course course){
-//        webElementActions.click(adminDashboard.addCourseButton)
-//                .sendKeys(courseNameField, course.getCourseName())
-//                .sendKeys(courseCategoryField,course.getCourseCategoryName())
-//                .sendKeys(descriptionField, course.getDescription())
-//                .click(saveAndSelectUsersButton);
-//        return this;
-//    }
+
+    public AddCoursePage addCourse(Course course){
+        webElementActions.click(adminDashboardPage.courses)
+                .click(addCourseButton)
+                .sendKeys(courseNameField, course.getCourseName())
+                .sendKeys(courseCategoryField,course.getCourseCategoryName())
+                .sendKeys(descriptionField, course.getDescription())
+                .click(saveAndSelectUsersButton);
+        return this;
+    }
 }
--- src/main/java/talentLMS/page/users/AddUserPage.java
+++ src/main/java/talentLMS/page/users/AddUserPage.java
@@ -3,14 +3,20 @@ package talentLMS.page.users;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 import talentLMS.entity.User;
 import talentLMS.page.base.BasePage;
 import talentLMS.page.dashboard.legacy.AdminDashboardPage;
 import talentLMS.utils.randomEntityUtils.RandomUserGenerator;
 
+import java.util.List;
+import java.util.Random;
+
 public class AddUserPage extends BasePage {
 
-    AdminDashboardPage dashboardPage = new AdminDashboardPage();
+    AdminDashboardPage adminDashboardPage = new AdminDashboardPage();
+    @FindBy(css = "div.btn-group > a.btn.btn-primary[href*='/user/create'")
+    public WebElement addUserButton;
 
     @FindBy(xpath = "//input[@name = 'name']")
     public WebElement firstName;
@@ -22,25 +28,44 @@ public class AddUserPage extends BasePage {
     public WebElement username;
     @FindBy(xpath = "//input[@name = 'password']")
     public WebElement password;
+    @FindBy(xpath = "//span[text()='Trainer-Type']/ancestor::a[contains(@class, 'select2-choice')]")
+    public WebElement userTypeButton;
+    @FindBy(name = "acl_user_type_id")
+    public WebElement userTypeOptions;
     @FindBy(xpath = "//input[@name ='submit_personal_details']")
     public WebElement addUserSubmitButton;
 
-//    public AddUserPage addNewUser(User user) {
-//        webElementActions.click(dashboardPage.addUserButton)
-//                .sendKeys(firstName, user.getFirstName())
-//                .sendKeys(lastName, user.getLastName())
-//                .sendKeys(email, user.getEmail())
-//                .sendKeys(username, user.getUserName())
-//                .sendKeys(password, RandomUserGenerator.generateStrongPassword(10))
-//                .click(addUserSubmitButton);
-//        return this;
-//    }
+
+    public AddUserPage addNewUser(User user) {
+        webElementActions.click(adminDashboardPage.users)
+                .click(addUserButton)
+                .sendKeys(firstName, user.getFirstName())
+                .sendKeys(lastName, user.getLastName())
+                .sendKeys(email, user.getEmail())
+                .sendKeys(username, user.getUserName())
+                .sendKeys(password, RandomUserGenerator.generateStrongPassword(10))
+                .click(addUserSubmitButton);
+        return this;
+    }
 
     public AddUserPage updateInfoOfUsers(String username) {
         driver.findElement(By.xpath("//tr[@role='row']/td/a/span[text()='" + username + "']")).click();
         firstName.clear();
+        lastName.clear();
         webElementActions.sendKeys(firstName, RandomUserGenerator.randomFirstName())
-                .click(new AddUserPage().addUserSubmitButton);
+                .sendKeys(lastName,RandomUserGenerator.randomLastName())
+                .click(userTypeOptions);
+        Select select = new Select(userTypeOptions);
+        // Получаем все доступные опции
+        List<WebElement> options = select.getOptions();
+
+        // Генерируем случайный индекс
+        Random random = new Random();
+        int randomIndex = random.nextInt(options.size());  // Генерирует от 0 до (размер списка - 1)
+
+        // Выбираем случайный элемент
+        select.selectByIndex(randomIndex);
+//                .click(addUserSubmitButton);
         return this;
     }
 }
