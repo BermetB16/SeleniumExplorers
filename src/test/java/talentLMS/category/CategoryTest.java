918428719e2b9cfd5d9922afbed45eb5a6d60290
BermetB <bermet160606@mail.com>
2025-02-11 16:52:37 +0600

    Category Commit

--- src/test/java/talentLMS/category/CategoryTest.java
+++ src/test/java/talentLMS/category/CategoryTest.java
@@ -1,33 +1,79 @@
 package talentLMS.category;
 
+import io.qameta.allure.Step;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import talentLMS.base.BaseTest;
 import talentLMS.fileUtils.ConfigReader;
+import talentLMS.utils.enums.Page;
+
+import java.time.Duration;
 
 public class CategoryTest extends BaseTest {
+    private String categoryName;
+    private int categoryPrice;
 
     @BeforeClass
-    public void authorization(){
-        driver.get("https://seleniumexplorers.talentlms.com/plus/login?redirect=%2Fdashboard");
+    public void setUp() {
+        browserManager.openURL("https://seleniumexplorers.talentlms.com/plus/login");
         loginPage.doLogin(ConfigReader.getProperty("userName"), ConfigReader.getProperty("password"));
+        adminDashboardModernPage.goToLegacyInterface();
+        pageNavigator.switchToPage(Page.CATEGORIES);
+        categoryName= "Baabar";
+        categoryPrice= 10;
     }
 
     @Test
+    @Step("Добавление категории")
     public void addCategoryTest(){
-//        String accountName = ConfigReader.getProperty("accountName");
-//        addCategoryPage.addNewCategory(accountName);
-    }
+        addCategoryPage.addNewCategory(categoryName, categoryPrice);
+        Assert.assertTrue(addCategoryPage.getCategorySuccessMessage().contains("Success! New category created."));
+        System.out.println("Success!!!");
+  }
 
     @Test
+    @Step("Добавление категории с пустым параметром")
+    public void addEmptyCategoryTest(){
+        addCategoryPage.addNewCategory("", 0);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement errorMessageElement = wait.until(ExpectedConditions.visibilityOf(addCategoryPage.errorNameRequired));
+        String actualErrorMessage = errorMessageElement.getText();
+        String expectedErrorMessage = "'Name' is required";
+        Assert.assertEquals(expectedErrorMessage, actualErrorMessage, "Error message text does not match");
+    }
+
+
+    @Test
+    @Step("Удаление категории")
     public void deleteCategoryTest(){
-        String accountName = ConfigReader.getProperty("accountName");
-        deleteCategoryPage.deleteCategory(accountName);
+        deleteCategoryPage.deleteCategory(categoryName);
+        Assert.assertFalse(deleteCategoryPage.isCategoryPresent(categoryName), "Category should not be present after deletion");
     }
 
     @Test
-    public void updateCategoryNameTest(){
-        String accountName = ConfigReader.getProperty("accountName");
-        addCategoryPage.updateCategory(accountName);
+    @Step("Редактироваание категории")
+    public void editCategoryNameTest(){
+        addCategoryPage.editCategory(categoryName);
+        Assert.assertTrue(addCategoryPage.getCategorySuccessUpdatedMessage(), "Message not found: Category update failed");
+    }
+
+    @Test
+    @Step("Отмена создания  категории")
+    public void cancelNewCategoryTest(){
+        addCategoryPage.cancelCategory(categoryName, categoryPrice);
+        Assert.assertFalse(deleteCategoryPage.isCategoryPresent(categoryName), "Category should not be present after deletion");
+        System.out.println("New category was SUCCESSFULLY CANCELED!!!");
+    }
+
+    @Test
+    @Step("Создание категории с символами")
+    public void CategoryWithCharactersTest() {
+        String categoryNameWithCharacters = "!@£$%qwert";
+        addCategoryPage.addNewCategory(categoryNameWithCharacters, categoryPrice);
+        Assert.assertTrue(addCategoryPage.getCategorySuccessCreatedMessage(), "Message not found: Category update failed");
     }
 }
