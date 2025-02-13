785af4c5f05692bbab9352c566fb4b6b444724ab
Nurda <nurdan.kazakbaev@gmail.com>
2025-02-13 15:16:44 +0600

    userstest

--- src/test/java/talentLMS/users/UsersTest.java
+++ src/test/java/talentLMS/users/UsersTest.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import talentLMS.base.BaseTest;
